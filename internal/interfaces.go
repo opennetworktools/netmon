7429f8e79e0c5e0d9a9af0ad119f6acbe33b71d2
Roopesh Saravanan <roopeshsaravanan.dev@gmail.com>
2024-01-22 19:04:11 +0530

    Added command to show interfaces

---
+++ internal/interfaces.go
@@ -0,0 +1,33 @@
+package internal
+
+import (
+	"fmt"
+	"log"
+	"github.com/google/gopacket/pcap"
+)
+
+func FindAllInterfaces() {
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, device := range devices {
+		fmt.Println(device.Name)
+	}
+}
+
+func FindAllInterfacesDescribe() {
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, device := range devices {
+		fmt.Println("\n", device.Name)
+		for _, address := range device.Addresses {
+			fmt.Println("- IP address: ", address.IP)
+			fmt.Println("- Subnet mask: ", address.Netmask)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"github.com/roopeshsn/netmon/internal"
+)
 
 func main() {
-	fmt.Println("netmon")
+	args := os.Args
+	if args[1] == "show" || args[1] == "sh" {
+		if args[2] == "interfaces" || args[2] == "int" {
+			// if args[3] == "describe" {
+			// 	internal.FindAllInterfacesDescribe()
+			// }
+			internal.FindAllInterfaces()
+		}
+	}
 }
