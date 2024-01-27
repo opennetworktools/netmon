ed5878d5ff8d949645242cc93cf0786faee9537f
Roopesh Saravanan <roopeshsaravanan.dev@gmail.com>
2024-01-27 17:45:05 +0530

    Handled 'invalid memory address' error and added util functions

--- internal/interfaces.go
+++ internal/interfaces.go
@@ -1,8 +1,10 @@
 package internal
 
 import (
+	"net"
 	"fmt"
 	"log"
+
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
 	"github.com/google/gopacket/layers"
@@ -14,6 +16,42 @@ type Address struct {
 	PORT string
 }
 
+func GetLocalIP() {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, iface := range interfaces {
+		// Exclude loopback and non-up interfaces
+		if iface.Flags&net.FlagUp == 0 || iface.Flags&net.FlagLoopback != 0 {
+			continue
+		}
+
+		addrs, err := iface.Addrs()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		for _, addr := range addrs {
+			ip, _, err := net.ParseCIDR(addr.String())
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			// Check if it's an IPv4 address
+			if ip.To4() != nil {
+				fmt.Println(ip.String())
+				return
+			}
+		}
+	}
+
+	fmt.Println("local IP address not found")
+}
+
 func FindAllInterfaces() {
 	devices, err := pcap.FindAllDevs()
 	if err != nil {
@@ -23,6 +61,25 @@ func FindAllInterfaces() {
 	for _, device := range devices {
 		fmt.Println(device.Name)
 	}
+}
+
+func FindInterfaceDescribe(name string) {
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, device := range devices {
+		if name == device.Name {
+			fmt.Println(device.Name)
+			for _, address := range device.Addresses {
+				fmt.Println("- IP address: ", address.IP)
+				fmt.Println("- Subnet mask: ", address.Netmask)
+			}
+			return
+		}
+	}
+	fmt.Printf("Unable to find interface with the name %s\n", name)
 }
 
 func FindAllInterfacesDescribe() {
@@ -46,7 +103,7 @@ func WatchInterface(name string) {
 		fmt.Printf("%s", err)
 	}
 	packetSource := gopacket.NewPacketSource(client.handler, client.handler.LinkType())
-	fmt.Println("SrcMAC            DestMAC           SrcIP          DestIP        SrcPort  DestPort")
+	fmt.Println("SrcMAC            DestMAC           SrcIP             DestIP        SrcPort  DestPort")
 	for packet := range packetSource.Packets() {
 		readPacket(packet)
 	}
@@ -58,20 +115,43 @@ func readPacket(packet gopacket.Packet) {
 	ipLayer := packet.Layer(layers.LayerTypeIPv4)
 
 	if tcpLayer != nil {	
-		ethHandler, _ := ethLayer.(*layers.Ethernet)
+		ethHandler, err := ethLayer.(*layers.Ethernet)
+		if err != true {
+			// fmt.Println("ethHandler")
+			return
+		}
 		sourceMAC := ethHandler.SrcMAC
 		destinationMAC := ethHandler.DstMAC
 
-		tcpHandler, _ := tcpLayer.(*layers.TCP)
+		tcpHandler, err := tcpLayer.(*layers.TCP)
+		if err != true {
+			// fmt.Println("tcpHandler")
+			return
+		}
 		sourcePort := tcpHandler.SrcPort
 		destinationPort := tcpHandler.DstPort
 		
-		httpHandler, _ := ipLayer.(*layers.IPv4)
+		httpHandler, err := ipLayer.(*layers.IPv4)
+		if err != true {
+			// fmt.Println("httpHandler")
+			return
+		}
 		sourceIP := httpHandler.SrcIP
 		destinationIP := httpHandler.DstIP
 	
 		sourceAddress := &Address{MAC: sourceMAC.String(), IP: sourceIP.String(), PORT: sourcePort.String()}
 		destinationAddress := &Address{MAC: destinationMAC.String(), IP: destinationIP.String(), PORT: destinationPort.String()}
-		fmt.Printf("%v %v %v %v %v %v\n", sourceAddress.MAC, destinationAddress.MAC, sourceAddress.IP, destinationAddress.IP, sourceAddress.PORT, destinationAddress.PORT)
+		fmt.Printf("%v %v %v -> %v %v %v\n", sourceAddress.MAC, destinationAddress.MAC, sourceAddress.IP, destinationAddress.IP, sourceAddress.PORT, destinationAddress.PORT)
+	}
+}
+
+func rDNS() {
+	domainNames, err := net.LookupAddr("13.232.193.230")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, domain := range domainNames {
+		fmt.Println(domain)
 	}
 }
--- main.go
+++ main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"os"
+	"fmt"
 
 	"github.com/roopeshsn/netmon/internal"
 )
 
 func main() {
 	args := os.Args
-	if args[1] == "show" || args[1] == "sh" {
+	if len(args) == 1 {
+		fmt.Println("No command entered!")
+	} else if args[1] == "show" || args[1] == "sh" {
 		if args[2] == "asn" {
 			internal.GetASN("81.2.69.142")
 		} else if args[2] == "country" || args[2] == "cc"  {
 			internal.GetCountry("81.2.69.142")
-		} else if args[2] == "interfaces" || args[2] == "int" {
+		} else if args[2] == "interface" || args[2] == "int" {
+			if len(args) >= 5 {
+				if args[3] == "describe" || args[3] == "des" {
+					internal.FindInterfaceDescribe(args[4])
+				}
+			}
+		} else if args[2] == "interfaces" || args[2] == "ints" {
 			if len(args) >= 4 {
 				if args[3] == "describe" || args[3] == "des" {
 					internal.FindAllInterfacesDescribe()
@@ -21,9 +30,12 @@ func main() {
 				return
 			}
 			internal.FindAllInterfaces()
+		} else if args[2] == "ip" {
+			internal.GetLocalIP()
 		}
-	}
-	if args[1] == "watch" {
+	} else if args[1] == "watch" {
 		internal.WatchInterface(args[2])
+	} else {
+		fmt.Printf("Command %v not found!\n", args[2])
 	}
 }
